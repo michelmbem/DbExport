b3c6d4c6cf9b57dacde658407bfaf7782a501ac4
Michel Mbem <michel.mbem@gmail.com>
2026-02-20 16:36:26 -0500

    Simplified `NullableConverters` by removing redundant fallback to an empty string

--- DbExport.Gui/Converters/NullableConverters.cs
+++ DbExport.Gui/Converters/NullableConverters.cs
@@ -5,6 +5,6 @@ namespace DbExport.Gui.Converters;
 public static class NullableConverters
 {
     public static readonly IValueConverter Int32 = new FuncValueConverter<int?, string?>(
-        i => i?.ToString() ?? string.Empty,
+        i => i?.ToString(),
         s => int.TryParse(s, out var i) ? i : null);
 }
