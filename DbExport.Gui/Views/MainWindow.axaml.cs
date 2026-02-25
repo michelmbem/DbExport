8f126e647407d7493a1a857e88e761f2fe2e70a3
Michel Mbem <mimbe@ulaval.ca>
2026-02-25 16:24:40 -0500

    Modernization of the title bar and Chrome window
    Redesigned main window with a custom title bar, relocation of the theme switcher button to the top bar, and improved system integration via ExtendClientArea properties. Added window movement by dragging the title bar. Adjusted styles and layout for an improved user experience.

--- DbExport.Gui/Views/MainWindow.axaml.cs
+++ DbExport.Gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace DbExport.Gui.Views;
 
@@ -7,5 +8,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnTitleBarPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        var currentPoint = e.GetCurrentPoint(this);
+
+        if (!currentPoint.Properties.IsLeftButtonPressed) return;
+
+        BeginMoveDrag(new PointerPressedEventArgs(this, currentPoint.Pointer, this, currentPoint.Position, default, default, default));
+    }
+
+    private void Grid_ActualThemeVariantChanged(object? sender, System.EventArgs e)
+    {
     }
 }
