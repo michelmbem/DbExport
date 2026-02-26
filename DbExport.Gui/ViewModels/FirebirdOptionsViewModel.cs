b7c7663c517306bdbe5db8a57a9af9ba517738be
Michel Mbem <michel.mbem@gmail.com>
2026-02-25 19:33:30 -0500

    Revert "FirebirdOptions view: grouped charset options in GUI combobox and style section headers"
    This reverts commit 832794d5542841ad84de89b53d4e8d42a3f2f84a.

--- DbExport.Gui/ViewModels/FirebirdOptionsViewModel.cs
+++ DbExport.Gui/ViewModels/FirebirdOptionsViewModel.cs
@@ -1,8 +1,4 @@
 using System.Collections.ObjectModel;
-using System.Linq;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using DbExport.Providers.Firebird;
 
@@ -14,7 +10,7 @@ public partial class FirebirdOptionsViewModel : ProviderOptionsViewModel
     private string dataDirectory = string.Empty;
     
     [ObservableProperty]
-    private ComboBoxItem? defaultCharSet;
+    private string defaultCharSet = "UTF8";
     
     [ObservableProperty]
     private int? pageSize = FirebirdOptions.PageSize;
@@ -25,15 +21,9 @@ public partial class FirebirdOptionsViewModel : ProviderOptionsViewModel
     [ObservableProperty]
     private bool overwrite = FirebirdOptions.Overwrite;
 
-    public FirebirdOptionsViewModel()
-    {
-        DefaultCharSet = CharacterSets[1];
-    }
-
     public static int DefaultPageSize => FirebirdOptions.PageSize;
 
-    public ObservableCollection<ComboBoxItem> CharacterSets { get; } =
-        [..FirebirdOptions.CharacterSets.Select(CharacterSet2ComboBoxItem)];
+    public ObservableCollection<string> CharacterSets { get; } = [..FirebirdOptions.CharacterSets];
 
     public override string Title => "Firebird options";
 
@@ -49,23 +39,8 @@ public partial class FirebirdOptionsViewModel : ProviderOptionsViewModel
             return new FirebirdOptions
             {
                 DataDirectory = DataDirectory,
-                DefaultCharSet = DefaultCharSet?.Content!.ToString()
+                DefaultCharSet = DefaultCharSet
             };
         }
     }
-
-    private static ComboBoxItem CharacterSet2ComboBoxItem(string charSet) =>
-        charSet.StartsWith("--")
-            ? new ComboBoxItem
-            {
-                Content = charSet[2..^2],
-                FontWeight = FontWeight.Bold,
-                Background = Brushes.Gray,
-                IsEnabled = false,
-            }
-            : new ComboBoxItem
-            {
-                Content = charSet,
-                Margin = new Thickness(10, 0, 0, 0),
-            };
 }
