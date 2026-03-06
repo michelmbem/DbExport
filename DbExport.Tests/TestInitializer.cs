e0f6a89ed5565ff0028185e4aa9674a8e460fae0
Michel Mbem <michel.mbem@gmail.com>
2026-03-05 19:36:38 -0500

    Added XML documentation for `TestInitializer` class, including static constructor and `EnsureInitialized` method. Enhanced clarity and purpose descriptions.

--- DbExport.Tests/TestInitializer.cs
+++ DbExport.Tests/TestInitializer.cs
@@ -2,10 +2,19 @@
 
 public static class TestInitializer
 {
+    /// <summary>
+    /// A static constructor that registers the DbProviderFactories for the test environment.
+    /// </summary>
     static TestInitializer()
     {
         Utility.RegisterDbProviderFactories();
     }
 
-    public static void EnsureInitialized() { }
+    /// <summary>
+    /// A dummy method that ensures that the static constructor has been executed.
+    /// </summary>
+    public static void EnsureInitialized()
+    {
+        // This method does nothing, but it ensures that the static constructor has been executed.
+    }
 }
